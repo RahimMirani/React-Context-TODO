93625b8f6393929582ac3f137cf465aa7b7d49e3
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2025-07-22 19:10:59 -0400

    Added update function

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,11 @@ function App() {
     setTodos((prev) => [{id: Date.now(), ...todo}, ...prev] )
   }
 
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
+
+  
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
     <div className="bg-[#172842] min-h-screen py-8">
