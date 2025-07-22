a1374d15a3e4451f9735895653ca07a40c6c9dd6
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2025-07-22 19:15:11 -0400

    Added delete function

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,10 @@ function App() {
     setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
   }
 
-  
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
     <div className="bg-[#172842] min-h-screen py-8">
